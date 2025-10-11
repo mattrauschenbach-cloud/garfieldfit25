e3edde86f02acf88859c63686b2fefcb5d47ddc8
mattrauschenbach-cloud <mattrauschenbach@gmail.com>
2025-10-11 14:54:09 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-// src/main.jsx
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./styles.css"
 import App from "./App"
+import "./styles.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
