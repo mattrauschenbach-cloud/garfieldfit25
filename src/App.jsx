dcfa3e3fc10597e43a3a7ec1f6198ecafe444e8b
mattrauschenbach-cloud <mattrauschenbach@gmail.com>
2025-10-11 15:20:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+// Layout & helpers
 import AppShell from "./components/AppShell"
 import ProtectedRoute from "./components/ProtectedRoute"
 import NotFound from "./components/NotFound"
 
+// Pages
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Log from "./pages/Log"
+import Members from "./pages/Members"       // owner can edit roles inline here
+import MyProfile from "./pages/MyProfile"   // lets you edit your name + (if allowed) role
 import Diag from "./pages/Diag"
 
 export default function App(){
@@ -13,14 +19,38 @@ export default function App(){
     <BrowserRouter>
       <AppShell>
         <Routes>
+          {/* Public */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/diag" element={<Diag />} />
-          <Route path="/log" element={
-            <ProtectedRoute>
-              <Log />
-            </ProtectedRoute>
-          }/>
+
+          {/* Protected */}
+          <Route
+            path="/log"
+            element={
+              <ProtectedRoute>
+                <Log />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/members"
+            element={
+              <ProtectedRoute>
+                <Members />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/me"
+            element={
+              <ProtectedRoute>
+                <MyProfile />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Alias & 404 */}
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
